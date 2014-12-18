a8aa9e444b24a782fd85ffc6723b5f74546fa7e8
vsandoria <v.sandoria@ilead-synapse.com>
2014-12-18 14:24:25 +0530

    failed case

--- UserTest.php
+++ UserTest.php
@@ -15,7 +15,7 @@ class UserTest extends PHPUnit_Framework_TestCase
 	
 	public function testTalk() {
         $user = new User();
-        $expected = "Hello world!";
+        $expected = "Hello";
         $actual = $user->talk();
         $this->assertEquals($expected, $actual);
     }
