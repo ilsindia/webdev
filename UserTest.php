b0fd5295bbd98b04c1a8d054ff5e169e8e637254
vsandoria <v.sandoria@ilead-synapse.com>
2014-12-18 13:25:43 +0530

    user test

---
+++ UserTest.php
@@ -0,0 +1,23 @@
+<?php
+require_once "User.php";
+class UserTest extends PHPUnit_Framework_TestCase
+{
+    protected $user;
+	
+	protected function setUp() {
+        $this->user = new User();
+        $this->user->setName("Tom");
+    }
+	
+	protected function tearDown() {
+        unset($this->user);
+    }
+	
+	public function testTalk() {
+        $user = new User();
+        $expected = "world!";
+        $actual = $user->talk();
+        $this->assertEquals($expected, $actual);
+    }
+}
+?>
